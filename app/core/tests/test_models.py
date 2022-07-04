e2a10106ca870f1739435f89fbd793da703acd25
Filip Mazic <mazicfilip.mf@gmail.com>
2022-07-04 21:11:12 +0200

    Fixed flake8

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -11,7 +11,10 @@ class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         email = 'test@example.com'
         password = 'testpass123'
-        user = get_user_model().objects.create_user(email=email, password=password)
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
@@ -24,7 +27,10 @@ class ModelTests(TestCase):
             ['test4@example.COM', 'test4@example.com'],
         ]
         for email, expected in sample_emails:
-            user = get_user_model().objects.create_user(email, 'sample123')
+            user = get_user_model().objects.create_user(
+                email,
+                'sample123'
+            )
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
@@ -32,6 +38,9 @@ class ModelTests(TestCase):
             get_user_model().objects.create_user('', 'test123')
 
     def test_create_superuser(self):
-        user = get_user_model().objects.create_superuser('test@example.com', 'test123')
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123'
+        )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
